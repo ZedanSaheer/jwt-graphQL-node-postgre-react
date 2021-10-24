3d89e8399412e8603d768e7a1af7507d13ef4bde
Zedan Saheer <zed.saheer5@gmail.com>
2021-10-25 01:31:51 +0530

    setting up graphql functions

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,46 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+/* import { createConnection } from "typeorm";
+import { User } from "./entity/User"; */
+import express from 'express';
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { UserResolver } from "./resolvers";
 
-createConnection().then(async connection => {
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+(async () => {
+    const app = express();
+    app.get("/", (_req, res) => res.send("Hello"));
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [UserResolver]
+        })
+    });
 
-    console.log("Here you can setup and run express/koa/any other framework.");
+    // added this line
+    await apolloServer.start();
 
-}).catch(error => console.log(error));
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(5000, () => {
+        console.log("server started!");
+    });
+})()
+
+// createConnection().then(async connection => {
+
+//     console.log("Inserting a new user into the database...");
+//     const user = new User();
+//     user.firstName = "Timber";
+//     user.lastName = "Saw";
+//     user.age = 25;
+//     await connection.manager.save(user);
+//     console.log("Saved a new user with id: " + user.id);
+
+//     console.log("Loading users from the database...");
+//     const users = await connection.manager.find(User);
+//     console.log("Loaded users: ", users);
+
+//     console.log("Here you can setup and run express/koa/any other framework.");
+
+// }).catch(error => console.log(error));
---
+++ src/resolvers.ts
@@ -0,0 +1,31 @@
+import { hash } from "bcryptjs";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { User } from "./entity/User";
+
+@Resolver()
+export class UserResolver {
+    @Query(() => String)
+    hello() {
+        return 'hi!';
+    }
+
+    @Mutation(()=>Boolean)
+    async register(
+        @Arg('email') email: string,
+        @Arg('password') password: string,
+    ) {
+
+        const hashedPassword = await hash(password,12);
+
+      try {
+        await User.insert({
+            email,
+            password : hashedPassword
+        });  
+      } catch (error) {  
+        console.log(error);
+        return false;
+      }
+      return true;     
+    }
+}
