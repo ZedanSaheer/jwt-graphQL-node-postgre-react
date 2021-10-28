fa24b1ea571d926b1db203382fffbe1bea7decc0
Zedan Saheer <zed.saheer5@gmail.com>
2021-10-28 15:01:46 +0530

    setup mikroORM for dynamic queries!

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,11 +1,15 @@
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import { MikroORM } from "@mikro-orm/core";
+import path from "path";
 
 export default {
+    migrations:{
+        path: path.join(__dirname,'./migrations'), 
+        pattern: /^[\w-]+\d+\.[tj]s$/, 
+    },
     entities:[Post],
     dbName:'redit',
-    
     user:'postgres',
     password:'123456',
     debug:!__prod__,
