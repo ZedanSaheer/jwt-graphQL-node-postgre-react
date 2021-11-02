da6b9cab5cf73d75ab37cba6c187d6a69c03c244
Zedan Saheer <zed.saheer5@gmail.com>
2021-11-03 00:32:20 +0530

    making log out mutation

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -2,111 +2,112 @@ import { MyContext } from "src/types";
 import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver } from "type-graphql";
 import argon2 from "argon2"
 import { User } from "../entities/User";
+import { COOKIE_NAME } from "src/constants";
 
 @InputType()
 class UsernameAndPasswordInput {
     @Field()
-    username : string
+    username: string
     @Field()
-    password : string
+    password: string
 }
 
 @ObjectType()
-class FieldError{
+class FieldError {
     @Field()
-    field : string;
+    field: string;
     @Field()
-    message : string;
+    message: string;
 }
 
 @ObjectType()
-class UserResponse{
-    @Field(()=>[FieldError],{nullable:true})
-    errors?:FieldError[];
+class UserResponse {
+    @Field(() => [FieldError], { nullable: true })
+    errors?: FieldError[];
 
-    @Field(()=>User,{nullable:true})
-    user?:User;
+    @Field(() => User, { nullable: true })
+    user?: User;
 }
 
 @Resolver()
 export class UserResolver {
 
-    @Query(()=>User,{nullable:true})
+    @Query(() => User, { nullable: true })
     async me(
-        @Ctx(){req,em}:MyContext
-    ){
-        if(!req.session.userId){
+        @Ctx() { req, em }: MyContext
+    ) {
+        if (!req.session.userId) {
             return null
         }
 
-        const user = await em.findOne(User,{id:req.session.userId});
+        const user = await em.findOne(User, { id: req.session.userId });
         return user;
     }
 
-    @Mutation(()=>UserResponse)
+    @Mutation(() => UserResponse)
     async register(
-        @Arg('options') options : UsernameAndPasswordInput,
-        @Ctx() {em} : MyContext
-    ):Promise<UserResponse>{
-        if(options.username.length<=2){
-            return{
-                errors:[
+        @Arg('options') options: UsernameAndPasswordInput,
+        @Ctx() { em }: MyContext
+    ): Promise<UserResponse> {
+        if (options.username.length <= 2) {
+            return {
+                errors: [
                     {
-                        field:"username",
-                        message : "Lenght must be greater than 2"
+                        field: "username",
+                        message: "Lenght must be greater than 2"
                     }
                 ]
             }
         }
-        if(options.password.length<=3){
-            return{
-                errors:[
+        if (options.password.length <= 3) {
+            return {
+                errors: [
                     {
-                        field:"password",
-                        message : "Lenght must be greater than 3"
+                        field: "password",
+                        message: "Lenght must be greater than 3"
                     }
                 ]
             }
         }
         const hasedPass = await argon2.hash(options.password)
-        const user = em.create(User ,
-             {username : options.username , password: hasedPass});
-       try {
-        await em.persistAndFlush(user);
-       } catch (error) {
-           //duplicate user
-          if(error.code==='23505' || error.detail.includes("Already exists")){
-              return {
-                  errors:[{
-                      field:"username",
-                      message:"Username already in use"
-                  }]
-              }
-          }
-       }
-        return {user,}
+        const user = em.create(User,
+            { username: options.username, password: hasedPass });
+        try {
+            await em.persistAndFlush(user);
+        } catch (error) {
+            //duplicate user
+            if (error.code === '23505' || error.detail.includes("Already exists")) {
+                return {
+                    errors: [{
+                        field: "username",
+                        message: "Username already in use"
+                    }]
+                }
+            }
+        }
+        return { user, }
     }
-    
-    @Mutation(()=>UserResponse)
+
+    @Mutation(() => UserResponse)
     async login(
-        @Arg('options') options : UsernameAndPasswordInput,
-        @Ctx() {em,req} : MyContext
-    ): Promise<UserResponse>{
-        const user = await em.findOne(User,{username:options.username.toLowerCase()});
-        if(!user){
-            return{
-                errors:[{
-                    field : 'username'
-                    ,message: 'Username does not exist!'
+        @Arg('options') options: UsernameAndPasswordInput,
+        @Ctx() { em, req }: MyContext
+    ): Promise<UserResponse> {
+        const user = await em.findOne(User, { username: options.username.toLowerCase() });
+        if (!user) {
+            return {
+                errors: [{
+                    field: 'username'
+                    , message: 'Username does not exist!'
                 }]
             }
         }
-        const valid = await argon2.verify(user.password , options.password);
-        if(!valid){
-            return{
-                errors:[{
-                    field : 'password'
-                    ,message: 'Password does not match!'
+        const valid = await argon2.verify(user.password, options.password);
+        if (!valid) {
+            return {
+                errors: [{
+                    field: 'password'
+                    , message: 'Password does not match!'
                 }]
             }
         }
@@ -115,7 +116,21 @@ export class UserResolver {
 
         return {
             user,
-        }
+        };
     }
-    
+
+    @Mutation(() => Boolean)
+    logout(
+        @Ctx() { req, res }: MyContext
+    ) {
+        return new Promise((resolve) => req.session.destroy((err) => {
+            res.clearCookie(COOKIE_NAME)
+            if (err) {
+                console.log(err);
+                resolve(false)
+                return
+            }
+            resolve(true)
+        }))
+    }
 }
